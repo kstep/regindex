f3d95cd43d82c7819fb216e57f6a432b7d349a99
Konstantin Stepanov <me@kstep.me>
2015-03-20 11:06:56 +0300

    stabilize it a little

--- src/lib.rs
+++ src/lib.rs
@@ -5,49 +5,27 @@ extern crate regex;
 use std::ops::Index;
 use regex::Regex;
 
-pub struct ReIdx<'a>(&'a Regex);
+pub struct ReIdx(Regex);
 
-impl<'a> ReIdx<'a> {
-    pub fn from_regex(re: &'a Regex) -> ReIdx<'a> {
+impl ReIdx {
+    pub fn new(re: Regex) -> ReIdx {
         ReIdx(re)
     }
 }
 
-trait ToReIdx {
-    fn to_regex_index<'a>(&'a self) -> ReIdx<'a>;
-}
-
-impl ToReIdx for Regex {
-    fn to_regex_index<'a>(&'a self) -> ReIdx<'a> {
-        ReIdx(self)
-    }
-}
-
-impl<'i> Index<ReIdx<'i>> for str {
+impl Index<ReIdx> for str {
     type Output = str;
     fn index<'a>(&'a self, index: &ReIdx) -> &'a str {
-        if let Some((start, end)) = index.0.find(self) {
-            &self[start..end]
-        } else {
-            &self[..0]
+        match index.0.find(self) {
+            Some((start, end)) => &self[start..end],
+            None => &self[..0]
         }
     }
 }
 
-/*
-impl<T: ToReIdx> Index<T> for str {
-    type Output = str;
-    #[inline(always)]
-    fn index<'a>(&'a self, index: &T) -> &'a str {
-        self[index.to_regex_index()]
-    }
-}
-*/
-
 #[test]
 fn test_index_by_regex() {
-    let re = regex!("^ab+");
-    let idx = re.to_regex_index();
-    assert_eq!("abb", &"abbcccdddd"[idx]);
-    assert_eq!("", &"acccdddd"[idx]);
+    let re = ReIdx::new(regex!("^ab+"));
+    assert_eq!("abb", &"abbcccdddd"[re]);
+    assert_eq!("", &"acccdddd"[re]);
 }
