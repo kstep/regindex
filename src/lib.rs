d3f780a068fe7caa20fefd328d7e1986862d5133
Konstantin Stepanov <me@kstep.me>
2015-03-20 13:10:14 +0300

    stability markers

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,18 @@ extern crate regex;
 use std::ops::Index;
 use regex::Regex;
 
+#[stable(since="0.1.0")]
 pub struct ReIdx(Regex);
 
+#[unstable]
 impl ReIdx {
+    #[unstable]
     pub fn new(re: Regex) -> ReIdx {
         ReIdx(re)
     }
 }
 
+#[stable(since="0.1.0")]
 impl Index<ReIdx> for str {
     type Output = str;
     fn index<'a>(&'a self, index: &ReIdx) -> &'a str {
